ef09af67fd1d2766cafaae8267f46a97cff8db01
kanokpit1234 <pranee251721@gmail.com>
2025-09-17 22:50:21 +0700

    Delete portfolio directory
    เฮ็ดจั่งได๋วะ

--- portfolio/script.js
+++
@@ -1,8 +0,0 @@
-// ดึง element
-const menuIcon = document.getElementById("menu-icon");
-const navbar = document.querySelector(".navbar");
-
-// เมื่อคลิก icon
-menuIcon.addEventListener("click", () => {
-    navbar.classList.toggle("active"); // เพิ่ม/ลบ class active
-});
